e25b40e8c17513148f00340987be65050c021a31
refat <refataalsakka@gmail.com>
2019-06-02 17:16:08 +0200

    new

--- App/View/users/common/footer.php
+++ App/View/users/common/footer.php
@@ -1,3 +1,4 @@
+        <script src="<?php echo assets('js/pages/jquery.js') ?>"></script>
         <script src="<?php echo assets('js/pages/' . $script . '.js') ?>"></script>
     </body>
 </html>
--- App/View/users/common/header.php
+++ App/View/users/common/header.php
@@ -5,6 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title><?php echo $title; ?></title>
+    <link rel="stylesheet" href="<?php echo assets('css/pages/bootstrap.css')?>" />
     <link rel="stylesheet" href="<?php echo assets('css/pages/' . $style . '.css')?>" />
 </head>
 <body>
