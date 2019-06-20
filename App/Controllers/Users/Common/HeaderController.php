442b4717c0198ea2a74af7d1887a4f1d288b42de
Refat Alsakka <refat838@gmail.com>
2019-06-20 11:15:38 +0200

    new

--- App/Controllers/Users/Common/HeaderController.php
+++ App/Controllers/Users/Common/HeaderController.php
@@ -11,11 +11,13 @@ class HeaderController extends Controller
         $title =  $this->html->getTitle() ?: '';
         $styles =  $this->html->getCss() ?: '';
         $login = $this->load->model('Login')->isLogged();
-
+        $codeId = $this->session->get('usercode');
+       
         $data = [
             'title' => $title,
             'styles' => $styles,
-            'login' => $login
+            'login' => $login,
+            'codeId' => $codeId,
         ];
         return $this->view->render('common\header', $data);
     }
