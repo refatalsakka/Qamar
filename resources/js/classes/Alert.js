995b143639fe4e4ded640f16e72222cc2edbccef
Refat Alsakka <ralsakka@cuxonline.de>
2020-02-11 09:00:09 +0100

    new

--- resources/js/classes/Alert.js
+++ resources/js/classes/Alert.js
@@ -11,13 +11,13 @@ class Alert {
   static formatElm(elm) {
     if (typeof elm === 'object') return elm;
 
-    elm = elm.trim(' ');
+    // elm = elm.trim(' ');
 
-    if (elm.indexOf(':') > -1) {
-      const [select, name] = elm.split(':');
-      if (select === 'id') return document.querySelector(`#${name.trim(' ')}`);
-      return document.querySelector(`.${name.trim(' ')}`);
-    }
+    // if (elm.indexOf(':') > -1) {
+    //   const [select, name] = elm.split(':');
+    //   if (select === 'id') return document.querySelector(`#${name.trim(' ')}`);
+    //   return document.querySelector(`.${name.trim(' ')}`);
+    // }
     // return document.querySelector(`.${elm}`);
   }
 
