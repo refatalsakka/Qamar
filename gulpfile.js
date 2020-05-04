a7ce8973a1148b7633ba3aacea9f0bf5b0113015
Refat Alsakka <ralsakka@cuxonline.de>
2020-05-04 14:58:06 +0200

    new

--- gulpfile.js
+++ gulpfile.js
@@ -13,7 +13,50 @@ const rename = require('gulp-rename');
 const imagesConvert = require('gulp-images-convert');
 const webpack = require('webpack');
 const webpackStream = require('webpack-stream');
-const webapckJsConfig = require('./webpack.config.js').webapckJsConfig;
+const path = require('path');
+
+const webapckJsConfig = {
+  mode: 'development',
+  entry: {
+    // Website JS Files
+    'website/script.js': './resources/js/website/script.js',
+    'website/pages/home.js': './resources/js/website/pages/home.js',
+    'website/pages/contact.js': './resources/js/website/pages/contact.js',
+    'website/pages/imprint.js': './resources/js/website/pages/imprint.js',
+    'website/pages/privacy.js': './resources/js/website/pages/privacy.js',
+    'website/pages/services.js': './resources/js/website/pages/services.js',
+    'website/pages/404.js': './resources/js/website/pages/404.js',
+
+    // Admin JS Files
+    'admin/script.js': './resources/js/admin/script.js',
+    'admin/pages/home.js': './resources/js/admin/pages/home.js',
+    'admin/pages/login.js': './resources/js/admin/pages/login.js',
+    'admin/pages/profile.js': './resources/js/admin/pages/profile.js',
+    'admin/pages/settings.js': './resources/js/admin/pages/settings.js',
+    'admin/pages/users/user.js': './resources/js/admin/pages/users/user.js',
+    'admin/pages/users/users.js': './resources/js/admin/pages/users/users.js',
+    'admin/pages/users/new.js': './resources/js/admin/pages/users/new.js',
+
+    // Libs JS Files
+    'libs/update.js': './resources/js/libs/update.js',
+  },
+  output: {
+    path: path.resolve(__dirname, 'public/'),
+    filename: '[name]',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.m?js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+        },
+      },
+    ],
+  },
+};
+
 
 // Folders 📁
 const TEMPLATE_DIR = 'template';
--- webpack.config.js
+++ webpack.config.js
@@ -1,51 +1,43 @@
 const path = require('path');
 
-const jsFiles = {
-  // Website JS Files
-  'website/script.js': './resources/js/website/script.js',
-  'website/pages/home.js': './resources/js/website/pages/home.js',
-  'website/pages/contact.js': './resources/js/website/pages/contact.js',
-  'website/pages/imprint.js': './resources/js/website/pages/imprint.js',
-  'website/pages/privacy.js': './resources/js/website/pages/privacy.js',
-  'website/pages/services.js': './resources/js/website/pages/services.js',
-  'website/pages/404.js': './resources/js/website/pages/404.js',
+module.exports = {
+  mode: 'development',
+  entry: {
+    // Website JS Files
+    'js/website/script.js': './resources/js/website/script.js',
+    'js/website/pages/home.js': './resources/js/website/pages/home.js',
+    'js/website/pages/contact.js': './resources/js/website/pages/contact.js',
+    'js/website/pages/imprint.js': './resources/js/website/pages/imprint.js',
+    'js/website/pages/privacy.js': './resources/js/website/pages/privacy.js',
+    'js/website/pages/services.js': './resources/js/website/pages/services.js',
+    'js/website/pages/404.js': './resources/js/website/pages/404.js',
 
-  // Admin JS Files
-  'admin/script.js': './resources/js/admin/script.js',
-  'admin/pages/home.js': './resources/js/admin/pages/home.js',
-  'admin/pages/login.js': './resources/js/admin/pages/login.js',
-  'admin/pages/profile.js': './resources/js/admin/pages/profile.js',
-  'admin/pages/settings.js': './resources/js/admin/pages/settings.js',
-  'admin/pages/users/user.js': './resources/js/admin/pages/users/user.js',
-  'admin/pages/users/users.js': './resources/js/admin/pages/users/users.js',
-  'admin/pages/users/new.js': './resources/js/admin/pages/users/new.js',
+    // Admin JS Files
+    'js/admin/script.js': './resources/js/admin/script.js',
+    'js/admin/pages/home.js': './resources/js/admin/pages/home.js',
+    'js/admin/pages/login.js': './resources/js/admin/pages/login.js',
+    'js/admin/pages/profile.js': './resources/js/admin/pages/profile.js',
+    'js/admin/pages/settings.js': './resources/js/admin/pages/settings.js',
+    'js/admin/pages/users/user.js': './resources/js/admin/pages/users/user.js',
+    'js/admin/pages/users/users.js': './resources/js/admin/pages/users/users.js',
+    'js/admin/pages/users/new.js': './resources/js/admin/pages/users/new.js',
 
-  // Libs JS Files
-  'libs/update.js': './resources/js/libs/update.js',
-};
-
-function main(entry) {
-  return {
-    mode: 'development',
-    entry,
-    output: {
-      path: path.resolve(__dirname, 'public/'),
-      filename: '[name]',
-    },
-    module: {
-      rules: [
-        {
-          test: /\.m?js$/,
-          exclude: /(node_modules|bower_components)/,
-          use: {
-            loader: 'babel-loader',
-          },
+    // Libs JS Files
+    'js/libs/update.js': './resources/js/libs/update.js',
+  },
+  output: {
+    path: path.resolve(__dirname, 'public/'),
+    filename: '[name]',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.m?js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
         },
-      ],
-    },
-  };
-}
-
-exports.webapckJsConfig = main(jsFiles);
-
-// module.exports = main(Object.assign(jsFiles));
+      },
+    ],
+  },
+};
