dbca484238d2fb147e64897278f6982b97afae40
Refat Alsakka <ralsakka@cuxonline.de>
2020-05-05 09:06:38 +0200

    edit file

--- gitty.js
+++ gitty.js
@@ -4,12 +4,20 @@ const args = process.argv;
 process.argv.splice(0, 2);
 const str = args.join(' ');
 
-function cbcommit(err) {
+function cbpush(err) {
   if (err) {
     console.log(err);
     return;
   }
   console.log('done');
+}
+
+function cbcommit(err) {
+  if (err) {
+    console.log(err);
+    return;
+  }
+  exec('git push', cbpush);
 }
 
 function cbadd(err) {
