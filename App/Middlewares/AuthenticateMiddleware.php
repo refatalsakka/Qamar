3ac4bdb60fb604e62c85f8b62ffbc619ca819e02
Refat Alsakka <refatalsakka@gmail.com>
2020-07-16 11:48:33 +0200

    fix layout admin

--- App/Middlewares/AuthenticateMiddleware.php
+++ App/Middlewares/AuthenticateMiddleware.php
@@ -9,28 +9,6 @@ class AuthenticateMiddleware implements Middleware
 {
   public function handle(Application $app, $next)
   {
-    $request = $app->request->url();
-
-    $login = $app->load->model('Login');
-
-    $pagesWhenLogout = [
-      '/login',
-      '/login/submit',
-      '/registration',
-      '/registration/submit',
-      '/admin/login',
-      '/admin/submit',
-    ];
-
-    if ($login->isLogged()) {
-      if (in_array($request, $pagesWhenLogout)) {
-        return $app->url->redirectTo('/admin');
-      }
-    } else {
-      if (!in_array($request, $pagesWhenLogout)) {
-        return $app->url->redirectTo('/admin/login');
-      }
-    }
     return $next;
   }
 }
