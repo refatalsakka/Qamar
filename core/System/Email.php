04612cd56b4f3d20547a4a9b2088b96554d11544
Refat Alsakka <refatalsakka@gmail.com>
2020-09-10 14:33:34 +0200

    email class

--- core/System/Email.php
+++ core/System/Email.php
@@ -39,43 +39,14 @@ class Email
 
   private function setUp()
   {
-    // $this->mail->SMTPDebug = Error::allowDisplayingError() ? SMTP::DEBUG_SERVER : 0;
-    // $this->mail->isSMTP();
-    // $this->mail->Host = $_ENV['EMAIL_HOST'];
-    // $this->mail->SMTPAuth = $_ENV['EMAIL_SMTPAUTH'];
-    // $this->mail->Username = $_ENV['EMAIL_USERNAME'];
-    // $this->mail->Password = $_ENV['EMAIL_PASSWORD'];
-    // $this->mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
-    // $this->mail->Port = $_ENV['EMAIL_PORT'];
-
-    $mail = new PHPMailer(true);
-
-    try {
-      //Server settings
-      $mail->SMTPDebug = SMTP::DEBUG_SERVER;                      // Enable verbose debug output
-      $mail->isSMTP();                                            // Send using SMTP
-      $mail->Host       = 'smtp.gmail.com';                    // Set the SMTP server to send through
-      $mail->SMTPAuth   = true;                                   // Enable SMTP authentication
-      $mail->Username   = 'refat838@gmail.com';                     // SMTP username
-      $mail->Password   = '8?W5?ENzd@qA9M$x8?W5?ENzd@qA9M$x';                               // SMTP password
-      $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;         // Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` encouraged
-      $mail->Port       = 587;                                    // TCP port to connect to, use 465 for `PHPMailer::ENCRYPTION_SMTPS` above
-
-      //Recipients
-      $mail->setFrom('refat838@gmail.com', 'Mailer');
-      $mail->addAddress('refatalsakka@gmail.com', 'Joe User');     // Add a recipient
-
-      // Content
-      $mail->isHTML(true);                                  // Set email format to HTML
-      $mail->Subject = 'Here is the subject';
-      $mail->Body    = 'This is the HTML message body <b>in bold!</b>';
-      $mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
-
-      $mail->send();
-      echo 'Message has been sent';
-  } catch (Exception $e) {
-      echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
-  }
+    $this->mail->SMTPDebug = Error::allowDisplayingError() ? SMTP::DEBUG_SERVER : 0;
+    $this->mail->isSMTP();
+    $this->mail->Host = $_ENV['EMAIL_HOST'];
+    $this->mail->SMTPAuth = $_ENV['EMAIL_SMTPAUTH'];
+    $this->mail->Username = $_ENV['EMAIL_USERNAME'];
+    $this->mail->Password = $_ENV['EMAIL_PASSWORD'];
+    $this->mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
+    $this->mail->Port = $_ENV['EMAIL_PORT'];
   }
 
   public function recipients($addresses, array $replayTo = [], $cc = null, $bcc = null)
