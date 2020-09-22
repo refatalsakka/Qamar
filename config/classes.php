7c87602fafe8ad7e5799198dce195eb27890c66e
Refat Alsakka <refatalsakka@gmail.com>
2020-09-22 15:11:43 +0200

    fixes

--- config/classes.php
+++ config/classes.php
@@ -1,19 +1,19 @@
 <?php
 
-    return [
-        'http'      =>  'System\\Http\\Http',
-        'response'  =>  'System\\Http\\Response',
-        'response'  =>  'System\\Http\\Response',
-        'upload'    =>  'System\\Http\\UploadeFile',
-        'route'     =>  'System\\Route',
-        'session'   =>  'System\\Session',
-        'cookie'    =>  'System\\Cookie',
-        'load'      =>  'System\\Loader',
-        'html'      =>  'System\\Html',
-        'db'        =>  'System\\Database',
-        'url'       =>  'System\\Url',
-        'view'      =>  'System\\View',
-        'hash'      =>  'System\\Hash',
-        'error'     =>  'System\\Error',
-        'email'     =>  'System\\Email',
-    ];
+return [
+    'http'      =>  'System\\Http\\Http',
+    'response'  =>  'System\\Http\\Response',
+    'response'  =>  'System\\Http\\Response',
+    'upload'    =>  'System\\Http\\UploadeFile',
+    'route'     =>  'System\\Route',
+    'session'   =>  'System\\Session',
+    'cookie'    =>  'System\\Cookie',
+    'load'      =>  'System\\Loader',
+    'html'      =>  'System\\Html',
+    'db'        =>  'System\\Database',
+    'url'       =>  'System\\Url',
+    'view'      =>  'System\\View',
+    'hash'      =>  'System\\Hash',
+    'error'     =>  'System\\Error',
+    'email'     =>  'System\\Email',
+];
