d7b517b22f11c0bf656ab45a00c48e5e42c31028
Refat Alsakka <refatalsakka@gmail.com>
2020-10-26 10:54:22 +0100

    fixes

--- app/Controllers/Website/LanguageController.php
+++ app/Controllers/Website/LanguageController.php
@@ -4,9 +4,9 @@ namespace app\Controllers\Website;
 
 use System\Controller as Controller;
 /**
-  * Language Controller
-  *
-  */
+ * Language Controller
+ *
+ */
 class LanguageController extends Controller
 {
     public function index($language)
